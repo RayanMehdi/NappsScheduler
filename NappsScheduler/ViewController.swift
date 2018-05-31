bc063786e897487eb1ab66e1d93a21eb7cfa18a9
Adeline <adeline-dumas@hotmail.fr>
2018-05-31 16:23:57 +0200

    ajout calendrier

--- NappsScheduler/ViewController.swift
+++ NappsScheduler/ViewController.swift
@@ -8,12 +8,45 @@
 
 import UIKit
 
+enum MyTheme {
+    case light
+    case dark
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "My Calender"
+        self.navigationController?.navigationBar.isTranslucent=false
+        self.view.backgroundColor=Style.bgColor
+        
+        view.addSubview(calenderView)
+        calenderView.topAnchor.constraint(equalTo: view.topAnchor, constant: 70).isActive=true
+        calenderView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -12).isActive=true
+        calenderView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 12).isActive=true
+        calenderView.heightAnchor.constraint(equalToConstant: 365).isActive=true
+        
+        let rightBarBtn = UIBarButtonItem(title: "Light", style: .plain, target: self, action: #selector(rightBarBtnAction))
+        self.navigationItem.rightBarButtonItem = rightBarBtn
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        calenderView.myCollectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    @objc func rightBarBtnAction(sender: UIBarButtonItem) {
+        Style.themeLight()
+        self.view.backgroundColor=Style.bgColor
+        calenderView.changeTheme()
+    }
+    
+    let calenderView: CalendarView = {
+        let v=CalendarView()
+        v.translatesAutoresizingMaskIntoConstraints=false
+        return v
+    }()
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
