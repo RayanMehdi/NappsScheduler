336132a1fddf26d16989b966a1980e35913ebab2
Thomas <thomas.chaboud@etu.univ-lyon1.fr>
2018-06-04 14:27:59 +0200

    Changement label date avec datePicker + outlets/actions TaskViewController

--- NappsScheduler/View/TaskViewController.swift
+++ NappsScheduler/View/TaskViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class TaskViewController: UITableViewController {
 
+    @IBOutlet weak var nameLabel: UITextField!
+    @IBOutlet weak var imageTableViewCell: UITableViewCell!
+    @IBOutlet weak var recurrenceTableViewCell: UITableViewCell!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var dateDatePicker: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +28,24 @@ class TaskViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func done(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func datePickerChanged(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        let strDate = dateFormatter.string(from: dateDatePicker.date)
+        dateLabel.text = strDate
+    }
+    
     /*
     // MARK: - Navigation
 
